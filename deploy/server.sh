a2ff18931d38a3eecf5b4ee5aaf7009fd8232cf8
CarlosRaloy <cgarcia@raloy.com.mx>
2024-12-20 17:59:15 -0600

    Deploy

--- deploy/server.sh
+++ deploy/server.sh
@@ -6,4 +6,6 @@ DJANGO_SETTINGS_MODULE=bot_google_chat.settings
 cd $DJANGODIR
 source venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-exec python manage.py runserver 10.150.8.30:8000
+exec uvicorn bot_google_chat.asgi:application --host 0.0.0.0 --port 8000 --workers 5
+
+#exec python manage.py runserver 10.150.8.30:8000
